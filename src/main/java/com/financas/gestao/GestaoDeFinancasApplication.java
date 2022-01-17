c461c1e903eac121f97e72fb4ed43ae2b14ffcf5
Daniel-R23 <danielima1023@gmail.com>
2022-01-17 18:11:07 -0300

    Initial commit

---
+++ src/main/java/com/financas/gestao/GestaoDeFinancasApplication.java
@@ -0,0 +1,13 @@
+package com.financas.gestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeFinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeFinancasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financas/gestao/GestaoDeFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financas.gestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
